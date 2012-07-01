24a1c79c6f5cacff98464ac3bfd7d2f78f6f4f7b
Mark Haskamp <mark.haskamp@pinnsg.com>
2012-07-01 13:52:57 -0400

    more/less links work for phone/table/desktop layouts

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
 
   $('body').on('click', '.more-link', function(event) {
     $more_link = $(event.target);
-    $more_link.addClass('hidden-tablet');
-    $more_link.next().removeClass('hidden-tablet');
+    $more_link.addClass('visible-desktop');
+    $more_link.next().removeClass('visible-desktop');
   });
 
   $('body').on('click', '.less-link', function(event) {
     $less_link = $(event.target);
-    $less_link.parent().addClass('hidden-tablet');
-    $less_link.parent().prev().removeClass('hidden-tablet');
+    $less_link.parent().addClass('visible-desktop');
+    $less_link.parent().prev().removeClass('visible-desktop');
   });
 });
