7146f6b920b8d480c2af803b19295f24ea88de30
Mark Haskamp <mark.haskamp@pinnsg.com>
2012-07-01 13:35:53 -0400

    less link works

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ $(document).ready(function() {
 
   $('body').on('click', '.more-link', function(event) {
     $more_link = $(event.target);
-    $more_link.removeClass('visible-tablet');
-    $more_link.hide();
+    $more_link.addClass('hidden-tablet');
     $more_link.next().removeClass('hidden-tablet');
+  });
+
+  $('body').on('click', '.less-link', function(event) {
+    $less_link = $(event.target);
+    $less_link.parent().addClass('hidden-tablet');
+    $less_link.parent().prev().removeClass('hidden-tablet');
   });
 });
